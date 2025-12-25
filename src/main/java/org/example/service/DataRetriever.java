938f95b722c315ba1dc39a12265307a29ced9520
Mathieu <hei.tafita.2@gmail.com>
2025-12-25 22:32:45 +0300

    feat: implement DataRetriever::findIngredients

--- src/main/java/org/example/service/DataRetriever.java
+++ src/main/java/org/example/service/DataRetriever.java
@@ -66,6 +66,52 @@ public class DataRetriever {
         }
     }
 
+    public List<Ingredient> findIngredients(int page, int size) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        int offset = (page - 1) * size;
+        String sql = """
+            SELECT i.id AS ingredient_id, i.name AS ingredient_name, i.price, i.category,
+                   d.id AS dish_id, d.name AS dish_name, d.dish_type
+            FROM ingredient i
+            LEFT JOIN dish d ON d.id = i.id_dish
+            LIMIT ? OFFSET ?
+        """;
+
+       Connection connection = dbConnection.getDBConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, size);
+            ps.setInt(2, offset);
+
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    Dish dish = null;
+                    int dishId = rs.getInt("dish_id");
+                    if (!rs.wasNull()) {
+                        String dt = rs.getString("dish_type");
+                        DishTypeEnum dishType = dt == null ? null : DishTypeEnum.valueOf(dt.toUpperCase());
+                        dish = new Dish(dishId, rs.getString("dish_name"), dishType);
+                    }
+
+                    CategoryEnum category = CategoryEnum.valueOf(rs.getString("category").toUpperCase());
+                    Ingredient ingredient = new Ingredient(
+                            rs.getInt("ingredient_id"),
+                            rs.getString("ingredient_name"),
+                            rs.getDouble("price"),
+                            category,
+                            dish
+                    );
+                    ingredients.add(ingredient);
+                    }
+            return ingredients;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            dbConnection.close(connection);
+        }
+    }
+
     private List<Ingredient> getIngredients(List<Ingredient> ingredients, PreparedStatement ps) throws SQLException {
         ResultSet resultSet = ps.executeQuery();
 
