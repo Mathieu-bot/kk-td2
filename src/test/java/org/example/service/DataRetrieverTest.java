d6a9fcda16c833b9f7e4a767ee617c1cede2f571
Mathieu <hei.tafita.2@gmail.com>
2025-12-31 13:10:51 +0300

    test: create DataRetrieverTest with not implemented test

---
+++ src/test/java/org/example/service/DataRetrieverTest.java
@@ -0,0 +1,32 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataRetrieverTest {
+
+    @Test
+    void findDishById() {
+    }
+
+    @Test
+    void findIngredients() {
+    }
+
+    @Test
+    void createIngredients() {
+    }
+
+    @Test
+    void saveDish() {
+    }
+
+    @Test
+    void findDishByIngredientName() {
+    }
+
+    @Test
+    void findIngredientsByCriteria() {
+    }
+}
