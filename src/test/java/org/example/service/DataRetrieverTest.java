0d17c8dff9fbcf23f729c49dd192b2a719a62beb
Mathieu <hei.tafita.2@gmail.com>
2025-12-31 20:26:10 +0300

    test: impl DataRetrieverTest::testFindDishById_existing and DataRetrieverTest::testFindDishById_notExisting

--- src/test/java/org/example/service/DataRetrieverTest.java
+++ src/test/java/org/example/service/DataRetrieverTest.java
@@ -1,14 +1,28 @@
 package org.example.service;
 
+import org.example.database.DBConnection;
+import org.example.model.Dish;
+import org.example.model.DishTypeEnum;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DataRetrieverTest {
+    DataRetriever dataRetriever = new DataRetriever(new DBConnection());
 
     @Test
-    void findDishById() {
+    void testFindDishById_existing() {
+        Dish dish = dataRetriever.findDishById(1);
+        assertEquals("Salade fraiche", dish.getName());
+        assertEquals(DishTypeEnum.START, dish.getDishType());
+        assertEquals(2, dish.getIngredients().size());
     }
+
+    @Test
+    void testFindDishById_notExisting() {
+        assertThrows(RuntimeException.class, () -> dataRetriever.findDishById(999));
+    }
+
 
     @Test
     void findIngredients() {
