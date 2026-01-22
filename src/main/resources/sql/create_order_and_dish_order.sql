c21eaf4f9dcedebb21ecd89d12d51a347bc4b153
Mathieu <hei.tafita.2@gmail.com>
2026-01-22 16:28:24 +0300

    chore: add script to create order and dish_order schema

---
+++ src/main/resources/sql/create_order_and_dish_order.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "order" (
+    id SERIAL CONSTRAINT order_pk PRIMARY KEY,
+    reference VARCHAR(255) NOT NULL,
+    creation_datetime TIMESTAMP NOT NULL
+);
+
+
+CREATE TABLE dish_order (
+    id SERIAL CONSTRAINT dish_order_pk PRIMARY KEY,
+    id_order INT NOT NULL,
+    id_dish INT NOT NULL,
+    quantity NUMERIC(10,2) NOT NULL,
+    CONSTRAINT fk_order FOREIGN KEY (id_order) REFERENCES "order"(id) ON DELETE CASCADE,
+    CONSTRAINT fk_dish FOREIGN KEY (id_dish) REFERENCES dish(id) ON DELETE CASCADE
+);
