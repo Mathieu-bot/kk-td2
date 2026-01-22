7723385287dcf1077c412e2d6a1d342509551171
Mathieu <hei.tafita.2@gmail.com>
2026-01-22 13:58:31 +0300

    chore(model): add StockValue class

--- src/main/java/org/example/model/StockValue.java
+++ src/main/java/org/example/model/StockValue.java
@@ -1,4 +1,20 @@
 package org.example.model;
 
 public class StockValue {
+
+    private final double quantity;
+    private final Unit unit;
+
+    public StockValue(double quantity, Unit unit) {
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
 }
