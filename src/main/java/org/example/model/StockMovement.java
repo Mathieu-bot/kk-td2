4750e8877c77036a779357b217e6a20eaf250fd0
Mathieu <hei.tafita.2@gmail.com>
2026-01-22 13:57:54 +0300

    chore(odel): add StockMovement class

---
+++ src/main/java/org/example/model/StockMovement.java
@@ -0,0 +1,34 @@
+package org.example.model;
+
+import java.time.Instant;
+
+public class StockMovement {
+
+    private final int id;
+    private final StockValue value;
+    private final MovementTypeEnum type;
+    private final Instant creationDateTime;
+
+    public StockMovement(int id, StockValue value, MovementTypeEnum type, Instant creationDateTime) {
+        this.id = id;
+        this.value = value;
+        this.type = type;
+        this.creationDateTime = creationDateTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public StockValue getValue() {
+        return value;
+    }
+
+    public MovementTypeEnum getType() {
+        return type;
+    }
+
+    public Instant getCreationDateTime() {
+        return creationDateTime;
+    }
+}
